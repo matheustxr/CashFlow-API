7dbc96442d44702651c91a5ac68bbdf06f3a0015
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-11-07 18:50:59 -0300

    create RequestRegisterExpenseJson, ResponsesRegisterJson, ReponsesErrorJson and initializated validate for register expense

--- src/CashFlow.API/Controllers/ExpensesController.cs
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.API.Controllers;
@@ -21,11 +22,15 @@ public class ExpensesController : ControllerBase
         } 
         catch (ArgumentException ex)
         {
-            return BadRequest(ex.Message);
+            var errorResponse = new ResponseErrorJson(ex.Message);
+
+            return BadRequest(errorResponse);
         }
-        catch 
+        catch
         {
-            return StatusCode(StatusCodes.Status500InternalServerError, "Unknown error");
+            var errorResponse = new ResponseErrorJson("Unknown error");
+
+            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
         }
     }
 }
--- src/CashFlow.Communication/Responses/ResponseErrorJson.cs
+++ src/CashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -2,4 +2,10 @@
 
 public class ResponseErrorJson
 {
+    public string ErrorMessage { get; set; } = string.Empty;
+
+    public ResponseErrorJson(string errorMessage)
+    {
+        ErrorMessage = errorMessage;
+    }
 }
