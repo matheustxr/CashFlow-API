67ca30b874f73a14f9ba9ffac370b05a8ea2bc8e
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-11-11 20:03:51 -0300

    created middleware for switch language errors

---
+++ src/CashFlow.API/Middlewares/LanguageMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace CashFlow.API.Middlewares;
+
+public class LanguageMiddleware
+{
+    private readonly RequestDelegate _next;
+    public LanguageMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+    public async Task Invoke (HttpContext context)
+    {
+        var language = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+        var languageInfo = new CultureInfo("en");
+
+        if (string.IsNullOrEmpty(language) == false)
+        {
+            languageInfo = new CultureInfo(language);
+        }
+
+        CultureInfo.CurrentCulture = languageInfo;
+        CultureInfo.CurrentUICulture = languageInfo;
+
+        await _next(context);
+    }
+}
