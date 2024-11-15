2d5ae3c1f134aae18acc39a4a4187ed1d72eb683
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-11-15 11:22:01 -0300

    created validator and tests unitys automatics

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,23 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CommonTestUnilities.Requests;
+using FluentValidation;
+
+namespace Validators.Tests.Expenses.Register;
+
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Sucess()
+    {
+        //Arange
+        var validator = new RegisterExpenseValidator();
+
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.True(result.IsValid);
+    }
+}
