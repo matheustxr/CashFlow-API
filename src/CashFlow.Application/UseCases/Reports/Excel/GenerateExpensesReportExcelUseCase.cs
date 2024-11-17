4cf1afbc4976a3a54a19107c7258fb287d25605a
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-11-17 17:25:00 -0300

    created generate table expenses for y/m and enpoint getTable per mounth

--- src/CashFlow.Application/UseCases/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ src/CashFlow.Application/UseCases/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -22,7 +22,7 @@ public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUs
             return [];
         }
 
-        var workbook = new XLWorkbook();
+        using var workbook = new XLWorkbook();
 
         workbook.Author = "Matheus Teixeira";
         workbook.Style.Font.FontSize = 12;
