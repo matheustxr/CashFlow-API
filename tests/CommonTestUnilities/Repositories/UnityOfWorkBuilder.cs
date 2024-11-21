dacd867a697db5387ff70b66c536432a18c08349
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-11-21 11:54:10 -0300

    starting implementation mocks

---
+++ tests/CommonTestUnilities/Repositories/UnityOfWorkBuilder.cs
@@ -0,0 +1,13 @@
+﻿using CashFlow.Domain.Repositories;
+using Moq;
+
+namespace CommonTestUtilities.Repositories;
+public class UnityOfWorkBuilder
+{
+    public static IUnityOfWork Build()
+    {
+        var mock = new Mock<IUnityOfWork>();
+
+        return mock.Object;
+    }
+}
---
+++ tests/UseCases.Test/Users/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,30 @@
+﻿using CashFlow.Application.UseCases.Users.Register;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+namespace UseCases.Test.Users.Register;
+public class RegisterUserUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestRegisterUserJsonBuilder.Build();
+        var useCase = CreateUseCase();
+
+        var result = await useCase.Execute(request);
+
+        result.Should().NotBeNull();
+        result.Name.Should().Be(request.Name);
+        result.Token.Should().NotBeNullOrWhiteSpace();
+    }
+
+    private RegisterUserUseCase CreateUseCase()
+    {
+        var mapper = MapperBuilder.Build();
+        var unitOfWork = UnityOfWorkBuilder.Build();
+        var writeRepository = UserWriteOnlyRepositoryBuilder.Build();
+
+        return new RegisterUserUseCase(mapper, null, null, writeRepository, null, unitOfWork);
+    }
+}
