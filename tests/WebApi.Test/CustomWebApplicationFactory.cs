22deda58eef72e23cfe91604d9a7c31b688a4e3e
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-11-23 10:07:41 -0300

    test: adding integration test for login

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,4 +1,6 @@
-﻿using CashFlow.Infrastructure.DataAccess;
+﻿using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Infrastructure.DataAccess;
+using CommonTestUtilities.Entities;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -7,11 +9,15 @@ using Microsoft.Extensions.DependencyInjection;
 namespace WebApi.Test;
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
+    private CashFlow.Domain.Entities.User _user;
+    private string _password;
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test")
             .ConfigureServices(services =>
             {
+                //configuração para iniciar o bd inMemory para testes
                 var provider = services.AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
 
                 services.AddDbContext<CashFlowDbContext>(config =>
@@ -19,6 +25,30 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
                     config.UseInMemoryDatabase("InMemoryDbForTesting");
                     config.UseInternalServiceProvider(provider);
                 });
+
+                //configuração para sempre inicar o bd com um usuario
+                var scope = services.BuildServiceProvider().CreateScope();
+                var dbContext = scope.ServiceProvider.GetRequiredService<CashFlowDbContext>();
+                var passwordEncripter = scope.ServiceProvider.GetRequiredService<IPasswordEncripter>();
+
+                StartDatabase(dbContext, passwordEncripter);
             });
+    }
+
+    //configuração para armazenar os dados gerados para iniciar o bd com um usuario
+    public string GetName() => _user.Name;
+    public string GetEmail() => _user.Email;
+    public string GetPassword() => _password;
+
+    //configuração para sempre inicar o bd com um usuario
+    private void StartDatabase(CashFlowDbContext dbContext, IPasswordEncripter passwordEncripter)
+    {
+        _user = UserBuilder.Build();
+        _password = _user.Password;
+        _user.Password = passwordEncripter.Encrypt(_user.Password);
+
+        dbContext.Users.Add(_user);
+
+        dbContext.SaveChanges();
     }
 }
--- tests/WebApi.Test/InlineData/CultureInlineDataTest.cs
+++ tests/WebApi.Test/InlineData/CultureInlineDataTest.cs
@@ -5,9 +5,7 @@ public class CultureInlineDataTest : IEnumerable<object[]>
 {
     public IEnumerator<object[]> GetEnumerator()
     {
-        yield return new object[] { "fr" };
         yield return new object[] { "pt-BR" };
-        yield return new object[] { "pt-PT" };
         yield return new object[] { "en" };
     }
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
